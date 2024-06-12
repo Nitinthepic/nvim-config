a149cfe9b788004e5fe64749fab7d7eec504dce4
Nitin Sharan <nitin.sharan03@gmail.com>
2024-06-11 23:10:08 -0700

    added new keymap for close pane with telescope

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Relink configuration.
-rm -rf "$HOME/.config/nvim"
-ln -sf "$PWD" "$HOME/.config/nvim"
-
-# Remove any existing artifacts.
-rm -rf ~/.local/state/nvim ~/.local/share/nvim ~/.cache/nvim
